eaff0efd71a694f036eda465adc11afaca6b5e45
Marie-Lynne Block <marielynne.block@gmail.com>
2018-03-21 05:50:37 +0100

    Add files via upload

---
+++ 003 Introduction to Python/python-intro.py
@@ -0,0 +1,27 @@
+# Python Intro
+
+################
+#  Exercise 1  #
+################
+
+print("Hello, Django Girls!")
+
+
+
+################
+#  Exercise 2  #
+################
+
+if 3 > 2:
+	print("It works!")
+
+
+
+################
+#  Exercise 3  #
+################
+
+if 5 > 2:
+	print("5 is indeed greater than 2")
+else:
+	print("5 is not greater than 2")
